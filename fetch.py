673fea2f288cd84a68ed38f5652f708fde0194fd
jpm <jmoriarty@gatech.edu>
2017-04-18 23:16:19 -0400

    Introduce Sector Information to Data

--- fetch.py
+++ fetch.py
@@ -38,11 +38,26 @@ Converts every attribute to numeric representation and introduces some new attri
 
 Returns a sorted list of financial attribute tuples, or an empty list if data missing.
 '''
-def clean_data(d):
+def clean_data(d, sector):
     exchanges   = {'NMS': 0, 'NYQ': 1}
     lets     = {'B': 10 ** 9, 'M': 10 ** 6, 'T': 10 ** 12}
 
+    sectors = { 'Consumer Discretionary':0,
+                'Consumer Staples':0,
+                'Energy':0,
+                'Financials':0,
+                'Health Care':0,
+                'Industrials':0,
+                'Information Technology':0,
+                'Materials':0,
+                'Real Estate':0,
+                'Telecommunication Services':0,
+                'Utilities':0}
+
+    sectors[sector] = 1
+
     dy = 'DividendYield'
+    ec = 'EPSEstimateCurrentYear'
     en = 'EPSEstimateNextYear'
     op = 'Open'
     pb = 'PriceBook'
@@ -58,21 +73,29 @@ def clean_data(d):
           'PercentChangeFromYearHigh',
           'PercentChangeFromYearLow' ]
     
-    let_conv = lambda d,a: float(d[a][:-1]) * lets[d[a][-1]] if search('[A-Z]',d[a]) else d[a] 
+    let_conv = lambda d,a: int(float(d[a][:-1]) * lets[d[a][-1]]) if search('[A-Z]',d[a]) else d[a] 
     per_conv = lambda d,a: float(d[a].strip('%')) / 100
 
+    # Letter to Numerals Conversion
+    for a in ['EBITDA', 'MarketCapitalization']: d[a] = let_conv(d,a)
+
     # Renaming Attributes
+    d['EPS']                        = d.pop('EarningsShare')
     d['PercentChangeFromYearHigh']  = d.pop('PercebtChangeFromYearHigh')
+    d['DaysVolume']                 = d.pop('Volume')
     d['.Symbol']                    = d.pop('symbol')
     d['.Name']                      = d.pop('Name')
 
     # Creating New Attributes
+    
+    dr = d['DaysRange'].split('-')
+    d['DaysRange'] = (float(dr[1]) - float(dr[0])) / float(d[pc])
+
+    d['EPSYearGrowthEstimate'] = (float(d[en]) / float(d[ec])) - 1 if d[en] else 0
+
     d['OneyrTargetChange']          = float(d[tp]) / float(d[pc]) - 1 if d[tp] else 0
     d['PercentChangeAfterHours']    = float(d[op]) / float(d[pc]) - 1 if d[op] else 0
     
-    # Letter to Numerals Conversion
-    for a in ['EBITDA', 'MarketCapitalization']: d[a] = let_conv(d,a)
-
     # Percent to Decimal Convertion
     for a in pa: d[a] = per_conv(d,a)
 
@@ -88,6 +111,10 @@ def clean_data(d):
     # Filter Attributes
     for a in [l.rstrip('\n') for l in open('attributes.txt') if '#' not in l]: del d[a]
 
+    # Add Sector Dummy Variables
+    if sector:
+        for s in sectors.keys(): d['Sector-' + s] = sectors[s]
+
     return sorted(list(d.items()))
 
 
@@ -97,14 +124,14 @@ Writes the cleaned current financial attributes of all SP 500 companies to {TODA
 def write_current_data():
     with open('data/' + strftime('%m-%d-%Y') + '.csv', 'w') as csv_file:
 
-        form = lambda d: list(zip(*clean_data(d)))
+        form = lambda symdata, sector: list(zip(*clean_data(symdata, sector)))
 
         w = writer(csv_file)
-        w.writerow(form(symbol_data('FDX'))[0])
+        w.writerow(form(symbol_data('FDX'), 'Industrials')[0])
 
         for security in sp_symbols():
             print(security[0])
-            row = form(symbol_data(security[0]))
+            row = form(symbol_data(security[0]), security[2])
             if row: w.writerow(row[1])
 
 
@@ -119,4 +146,4 @@ if __name__ == '__main__':
     if args.data: write_current_data()
     if args.list: print(*sp_symbols(), sep='\n')
     if args.sym : print(*sorted(symbol_data(args.sym).items()), sep='\n')
-    if args.SYM : print(*clean_data(symbol_data(args.SYM)), sep='\n')
+    if args.SYM : print(*clean_data(symbol_data(args.SYM), None), sep='\n')
