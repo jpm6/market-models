53ee1f92ad8d8e61268272c9eaf761ba91a404e9
jpm <jmoriarty@gatech.edu>
2017-04-25 10:48:08 -0400

    Vary Losses & Penalties x100

--- models/svm.py
+++ models/svm.py
@@ -6,19 +6,38 @@ import pandas as pd
 trd = '../data/split/train-data.csv'
 trl = '../data/split/train-labels.csv'
 
-X = np.asarray(pd.read_csv(trd))
-y = np.asarray(pd.read_csv(trl)).reshape(295,)
-
-clf = SGDClassifier(loss="hinge", penalty="l2")
-clf.fit(X, y)
-
 ted = '../data/split/test-data.csv'
 tel = '../data/split/test-labels.csv'
+
+X = np.asarray(pd.read_csv(trd))
+y = np.asarray(pd.read_csv(trl)).reshape(295,)
 
 d = np.asmatrix(pd.read_csv(ted))
 l = np.asarray(pd.read_csv(tel))
 
-c = 0
+losses    = ['hinge', 'log']
+penalties = ['l1', 'l2', 'elasticnet']
 
-for u,v in list(zip(d,l)):
-    if clf.predict(u) == v: c += 1 
+for loss in losses:
+    for penalty in penalties:
+
+        results = []
+
+        for i in range(100):
+
+            clf = SGDClassifier(loss=loss, penalty=penalty, shuffle=True)
+
+            clf.fit(X, y)
+
+            c = 0.0
+
+            for u,v in list(zip(d,l)):
+                if clf.predict(u) == v: c += 1 
+
+            results.append(c / 127)
+
+
+        print(loss, penalty)
+        print(sum(results) / 100, '\n')
+
+
